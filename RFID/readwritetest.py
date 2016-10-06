7d3c9d79e8ddcccd62d2945cc4118d8a3affab3c
Jezzar <gm.rallos@gmail.com>
2016-10-06 20:21:26 +1000

    Testing Changes

---
+++ RFID/readwritetest.py
@@ -0,0 +1,15 @@
+from libRFID import *
+import Adafruit_PN532 as PN532
+
+#Configure the Reader
+pn532 = initialise_RFID(18, 25, 23, 24)
+
+#Read Card ID and Value in Memeory Block 18
+
+
+
+#Write to Memory Block 1
+
+
+
+#Read from Memory Block 1
