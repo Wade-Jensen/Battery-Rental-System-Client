2e5116fba5f7f3cd1b2bc3b34086834c575c77b7
Gerard Rallos <gm.rallos@gmail.com>
2016-10-08 12:36:11 +1000

    Mtest

--- Client Server Contact/MachineConfigTest.py
+++ Client Server Contact/MachineConfigTest.py
@@ -11,11 +11,11 @@ batCap = 3
 
 #Create URL string from information
 
-contactString = "52.65.119.99:9000/test/name/%s/time/%s/capacity/%s" % (name,ts,batCap)
+contactString = "http://52.65.119.99:9000/test/name/%s/time/%s/capacity/%s" % (name,ts,batCap)
 print(contactString)
 
 #Contact URL and retrieve JSON object
-req = urllib2.urlopen('52.65.119.99:9000/test/name/Gerard/time/1475406647/capacity/3')
+req = urllib2.urlopen(contactString)
 res = req.read()
 print('JSON Object')
 print(res)
